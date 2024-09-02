23335731761182bbce73941ae0bebe9e37b8ec94
Nehanshu <nehanshugaidhani@gmail.com>
2024-09-02 15:32:32 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -3,14 +3,9 @@ import styles from "./page.module.css";
 import Header from "../Components/Navbar.js"
 import Mainpage from "../Components/Homepage.js"
 
-import Appdes from "../Components/Apppage.js"
+
 import Videopage from "../Components/Videopage.js"
-import Cardpage1 from "../Components/Cardpage.js"
-import Review from "../Components/Review.js"
-import ComplianceSection from "@/Components/Security";
-import Platform from "../Components/Platform.js"
-import Chat from "../Components/Formpage"
-import Footer from "../Components/Footerpage"
+
 export default function Home() {
   return (
   
